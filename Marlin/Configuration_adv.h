76ef79df581425365d6b1277bcfc0cd85f5022f8
Danny Burnett <danny.j.burnett@gmail.com>
2024-05-22 15:22:02 +0200

    Update Marlin/Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2996,7 +2996,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Y)
-    #define Y_CURRENT       580
+    #define Y_CURRENT       800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
